4e68c33c7028455449efb35506c1eb4dc8384d21
Manoel Neto <manoelcontatosi@gmail.com>
2020-04-29 13:24:43 -0300

    normalize coordenadas

--- Slam/SlamFlashBack/extractor.py
+++ Slam/SlamFlashBack/extractor.py
@@ -1,19 +1,35 @@
 import cv2
 import numpy as np
+np.set_printoptions(suppress=True)
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
 from skimage.transform import EssentialMatrixTransform
 
+# turn [[x,y]] -> [[x,u,1]]
+def add_ones(x):
+  return np.concatenate([x, np.ones((x.shape[0],1))],axis=1)
 
 class Extractor(object):
      GX = 16//2
      GY = 12//2
 
-     def __init__(self):
-        self.orb = cv2.ORB_create(100)
+     def __init__(self, k):
+        self.orb = cv2.ORB_create()
         self.bf  =  cv2.BFMatcher(cv2.NORM_HAMMING)
         self.last = None
+        self.k = k
+        self.Kinv = np.linalg.inv(self.k)
      
+     def normalize(self, pts):
+       return np.dot(self.Kinv, add_ones(pts).T).T[:, 0:2] 
+
+     def denormalize(self,pt):
+       ret = np.dot(self.k, np.array([pt[0], pt[1], 1.0]).T)
+       #ret /= ret[2]
+       return int(round(ret[0])),int(round(ret[1]))
+      
+
+
      def extractor(self,img):
          # detection
          feats = cv2.goodFeaturesToTrack(np.mean(img,axis=2).astype(np.uint8),3000, qualityLevel=0.01,minDistance=3)
@@ -34,17 +50,20 @@ class Extractor(object):
          #filter
          if len(ret) > 0:
             ret = np.array(ret)
-          
              # normalize coords: subtract to move to 0
-            ret[:, :,0] -= img.shape[0]//2
-            ret[:, :,1] -= img.shape[1]//2
+            ret[:, 0, :] =  self.normalize(ret[:, 0, :])
+            ret[:, 1, :] =  self.normalize(ret[:, 1, :])
+
             model, inliers = ransac((ret[:, 0],ret[:, 1]),
                                   #EssentialMatrixTransform,
                                   FundamentalMatrixTransform,
                                   min_samples=8,
-                                  residual_threshold=0.01, 
+                                  residual_threshold=1, 
                                   max_trials=100)
-            print(sum(inliers))
+            ret = ret[inliers]
+            
+           #s,v,d = np.linalg.svd(model.params)
+            #print(v)
          # retunr
          self.last = {'kps':kps,'des':des}
          return ret
